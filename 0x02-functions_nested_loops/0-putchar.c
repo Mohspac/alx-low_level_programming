5f95fa8f41667d953daa9a0bd292af21693d9b71
Olawale <ola.wale9@gmail.com>
2022-09-13 00:28:45 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *Return: 0 always
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putcar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
