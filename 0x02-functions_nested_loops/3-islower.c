5637299645e7646068dd3c79079e069c1fed2698
Olawale <ola.wale9@gmail.com>
2022-09-13 03:09:08 -0700

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,11 @@
 /**
  * main - _islower - to check if character is lowercase
  * @c: a character argument
+ * Description: checks for lowercase character
  * Return: returns 1 and 0
  */
-int _islower(int c)
+
+int main(void)
 {
 	return (c >= 'a' && c <= 'z');
 }
