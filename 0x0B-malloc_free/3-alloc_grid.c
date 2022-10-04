9de39093bb27accef5ed5bb1362e5dfe01c07a42
Olawale <ola.wale9@gmail.com>
2022-10-04 06:06:33 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
  * @width: width of the array.
@@ -8,7 +7,6 @@
  *
  * Return: pointer of an array of integers
  */
-
 int **alloc_grid(int width, int height)
 {
 	int **gridout;
